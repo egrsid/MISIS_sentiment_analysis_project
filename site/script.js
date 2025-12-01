1e3f72cf3ca92cf6a672e92440c53bad96c11041
Pinkielethant <val3rca@yandex.ru>
2025-12-01 10:04:48 +0300

    btnmodel

--- site/script.js
+++ site/script.js
@@ -89,3 +89,19 @@ document.addEventListener('click', function(event) {
         closeStoryBar();
     }
 });
+function toggleDropdown() {
+    const dropdown = document.getElementById("modelList");
+    dropdown.classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.model-select-button') && !event.target.closest('.model-select-button')) {
+        const dropdowns = document.getElementsByClassName("dropdown-content");
+        for (let i = 0; i < dropdowns.length; i++) {
+            const openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
